6e16d252b554677560a5d9f9a53934b6397c0676
le nghia <tro.ngnghia@yahoo.com.vn>
2017-06-02 16:47:58 +0000

    ssss

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ app.get('/', function(req, res) {
 });
 
 
-
-
+// GET longurl 
 app.get('/new/:longurl(*)', function(req,res){
   
       var longurl = req.params.longurl;
@@ -32,9 +31,10 @@ app.get('/new/:longurl(*)', function(req,res){
   } 
     console.log('connect to the mongoDB');
 
+//new links will be stored in the collection 'links'
 var collection = db.collection('links')
 
-// function 
+// function  add newlink
 var newlink = function (db, callback){
               if (err) {
     console.log('Error:', err);
@@ -50,12 +50,14 @@ var newlink = function (db, callback){
       
     newlink (db, function(){
         
-    
     var shortcode =  shortid.generate();
-    console.log('code '+shortcode)
+
     var link = {url : longurl, short:shortcode};
+    
       collection.insert(link);
+      
       res.json({original_url: longurl, short_url: 'https://shielded-sea-69229.herokuapp.com/'+shortcode})
+      
       db.close();
     });
       
@@ -72,7 +74,49 @@ var newlink = function (db, callback){
 
 });
 
+//GET short url -> redirection
+app.get('/new/:short', function(req,res){
+        var shortcode = req.params.short;
 
+   MongoClient.connect(url, function (err, db) {
+      
+      if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } 
+      console.log('connect to the mongoDB');
+      
+      //fetch data from the collection 'links'
+var collection = db.collection('links')
+
+      // function  find link
+var findlink = function (db, callback){
+              if (err) {
+    console.log('Error:', err);
+  } 
+        return callback(db);
+    };
+
+
+findlink(db, function() {
+    collection.findOne({short:shortcode},{url:1,_id:0}, function(doc){
+          if (doc !=null ) {
+              
+        console.log('FOUND a doc!!');  
+        
+        // redirect to the corresponding original url
+        res.redirect(doc.url)
+} else {
+            console.log('NOT FOUND a doc!!');  
+            // return error JSON
+res.json({error:'NOT FOUND THIS SHORT LINK IN THE DATABASE'})
+}
+            })
+    
+}
+    )
+   });
+  
+});
 
 app.listen(port, function(){
     console.log('server is listening ' +port);
